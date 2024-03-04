d0f363fd8b322bdbd3bf64a4b39b804e17cefffb
elozo <elozovetalyapynova05@gmail.com>
2024-03-04 22:43:46 +0700

    Корректирование главной страницы

--- app/src/main/java/com/example/course_project/MainActivity.java
+++ app/src/main/java/com/example/course_project/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.course_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Handler;
+import android.view.animation.AnimationUtils;
+import android.widget.ViewAnimator;
 import android.os.Bundle;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Настройка баннера
+        ViewAnimator viewAnimator = findViewById(R.id.ViewAnimator);
+        // Устанавливаем анимации перехода
+        viewAnimator.setInAnimation(AnimationUtils.loadAnimation(this, android.R.anim.slide_in_left));
+        viewAnimator.setOutAnimation(AnimationUtils.loadAnimation(this, android.R.anim.slide_out_right));
+        Handler handler = new Handler();
+        Runnable runnable = new Runnable() {
+            int i = 0;
+            public void run() {
+                viewAnimator.showNext();
+                i++;
+                if (i >= viewAnimator.getChildCount()) {
+                    i = 0;
+                }
+                handler.postDelayed(this, 3000); // Задержка в миллисекундах (3000 = 3 секунды)
+            }
+        };
+
+        // Добавляем обработчики для ручного перелистывания
+        viewAnimator.setOnClickListener(v -> {
+            viewAnimator.showNext();
+            handler.removeCallbacks(runnable); // Удаляем предыдущий автоматический Runnable
+            handler.postDelayed(runnable, 3000); // Запускаем новый автоматический Runnable
+        });
+
+        handler.postDelayed(runnable, 3000); // Запускаем первое переключение через 3 секунды
     }
 }
